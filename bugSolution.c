7b13f01cd0a11eb3f86695f27e127b4a69ce901a
BugHunter-X <askzenai@gmail.com>
2024-12-21 14:30:04 +0100

    Add bugSolution.c file

---
+++ bugSolution.c
@@ -0,0 +1,8 @@
+int main() {
+  int x = 10;
+  int *ptr = NULL; // Initialize pointer to NULL
+  ptr = &x; // Assign the address of x to ptr after initialization
+  *ptr = 20;
+  printf("%d\n", x);
+  return 0;
+}
